b25853a47a42db40102e46f20aaefd41a6bf200b
vitaly <al-fa@meta.ua>
2016-08-17 11:54:46 +0300

    subtotal

--- theme_modeling.py
+++ theme_modeling.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-from gensim import corpora, models
+# import matplotlib.pyplot as plt
+from scipy.spatial import distance
+from gensim import corpora, models, matutils
 
 # Associated Press news data http://www.cs.princeton.edu/~blei/lda-c/ap.tgz
 corpus = corpora.BleiCorpus('./data/ap/ap.dat', './data/ap/vocab.txt')
@@ -10,3 +11,18 @@ doc = corpus.docbyoffset(0)
 topics = model[doc]
 # document associated topics (topic index/weight pairs)
 # print(topics)
+
+# theme matrix
+topics = matutils.corpus2dense(model[corpus], num_terms=model.num_topics)
+# pair distance
+pairwise = distance.squareform(distance.pdist(topics))
+# set max value to diagonal matrix elements
+largest = pairwise.max()
+for ti in range(len (topics)):
+    pairwise[ti, ti] = largest + 1
+
+
+# find closest doc (closest neighbor classification)
+def closest_to(doc_id):
+    return pairwise[doc_id].argmin()
+
